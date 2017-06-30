c94d79746789106709b990a81ccfe17ec503c6cf
collen <collen@astronomer.io>
2017-06-30 09:52:03 -0400

    added boiler plate test.js file:

---
+++ test/test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var Analytics = require('@astronomerio/analytics.js-core').constructor;
+var integration = require('@astronomerio/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
+var Netmining = require('../lib/index');
+
+describe('Netmining', function() {
+    var analytics;
+    var Netmining;
+    var options = {};
+
+    beforeEach(function() {
+        analytics = new Analytics();
+        Netmining = new Netmining(options);
+        analytics.use(Netmining);
+        analytics.use(tester);
+        analytics.add(Netmining);
+    });
+
+    afterEach(function() {
+        analytics.restore();
+        analytics.reset();
+        Netmining.reset();
+        sandbox();
+    });
+
+    it('should have the correct settings', function() {
+    });
+
+    describe('after loading', function() {
+        beforeEach(function(done) {
+            analytics.stub(Netmining, 'load');
+            analytics.once('ready', done);
+            analytics.initialize();
+        });
+    });
+});
